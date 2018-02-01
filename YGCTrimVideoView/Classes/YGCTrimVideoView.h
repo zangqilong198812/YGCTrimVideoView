deab01c38c3aeb601536ab1820c402ac7747b5b1
Qilong Zang <qilong.zang@ITCN000105-MAC.local>
2018-02-01 16:56:07 +0800

    add comment

--- YGCTrimVideoView/Classes/YGCTrimVideoView.h
+++ YGCTrimVideoView/Classes/YGCTrimVideoView.h
@@ -8,20 +8,45 @@
 
 #import <UIKit/UIKit.h>
 
+@protocol YGCTrimVideoViewDelegate<NSObject>
+
+@end
+
 @interface YGCTrimVideoView : UIView
 
+/*
+ * change the sidebar width
+ */
 @property (nonatomic, assign) CGFloat sidebarWidth;
 
+/*
+ * change the left sidebar Image
+ */
 @property (nonatomic, strong) UIImage *leftSidebarImage;
 
+/*
+ * change the right sidebar Image
+ */
 @property (nonatomic, strong) UIImage *rightSidebarImage;
 
+/*
+ * change the center range Image
+ */
 @property (nonatomic, strong) UIImage *centerRangeImage;
 
+/*
+ * change the maximum duration of video
+ */
 @property (nonatomic, assign) NSTimeInterval maxSeconds;
 
+/*
+ * change the minimum duration of video
+ */
 @property (nonatomic, assign) NSTimeInterval minSeconds;
 
+/*
+ * change the left and right side maskView when slider range decrease
+ */
 @property (nonatomic, strong) UIColor *maskColor;
 
 - (id)initWithFrame:(CGRect)frame
